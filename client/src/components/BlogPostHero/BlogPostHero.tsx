6f34090eeb30c2397062973baea429233412af51
Sascha Ringström <sascha.ringstrom@redmind.se>
2021-05-05 18:57:36 +0200

    improved blog responsiveness

--- client/src/components/BlogPostHero/BlogPostHero.tsx
+++ client/src/components/BlogPostHero/BlogPostHero.tsx
@@ -1,8 +1,9 @@
 import moment from 'moment'
 import React, { FunctionComponent, useContext } from 'react'
+import { useMediaQuery } from 'react-responsive'
 import { BlogPostContext } from '../../templates/blogPost'
 import { ContentContainer, Spacer } from '../../theme/base'
-import { Body2, Headline1, Title1 } from '../../theme/typography'
+import { Body2, Headline1, Headline4, Title1 } from '../../theme/typography'
 import * as S from './styled'
 
 type Props = {}
@@ -16,18 +17,21 @@ const BlogPostHero: FunctionComponent<Props> = () => {
     },
     createdAt,
   } = useContext(BlogPostContext)
+  const isMobile = useMediaQuery({ query: '(max-width: 1224px)' })
 
   const date = moment(createdAt).format('D MMMM YYYY')
+  const Title = isMobile ? Headline1 : Title1
+  const Preamble = isMobile ? Headline4 : Headline1
 
   return (
     <S.Container>
       <ContentContainer>
         <Spacer exact={130} mobile={40} />
-        <Title1 onAccent>{headline}</Title1>
+        <Title onAccent>{headline}</Title>
         <Spacer h12 />
-        <Headline1 onAccent className="tide-sans-300">
+        <Preamble onAccent className="tide-sans-300">
           {preamble}
-        </Headline1>
+        </Preamble>
         <Spacer h40 />
         <Body2 onAccent>{date}</Body2>
         <Spacer exact={100} mobile={40} />
