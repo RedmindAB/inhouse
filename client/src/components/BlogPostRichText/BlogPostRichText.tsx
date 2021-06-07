ef624afce60079a7c2d408370cc64cbf443441d8
Sascha Ringström <sascha.ringstrom@redmind.se>
2021-06-07 17:52:37 +0200

    added assets and bold texts

--- client/src/components/BlogPostRichText/BlogPostRichText.tsx
+++ client/src/components/BlogPostRichText/BlogPostRichText.tsx
@@ -1,7 +1,7 @@
 import { BLOCKS, MARKS } from '@contentful/rich-text-types'
 import React, { FunctionComponent, useContext } from 'react'
 import { BlogPostContext } from '../../templates/blogPost'
-import { Spacer } from '../../theme/base'
+import { ContentContainer, Spacer } from '../../theme/base'
 import {
   Body2,
   Headline1,
@@ -43,25 +43,37 @@ const options = {
     ),
   },
   renderNode: {
-    [BLOCKS.HEADING_1]: (node, children) => (
-      <Headline1 onAccent>{children}</Headline1>
+    [BLOCKS.HEADING_1]: (node, children, bold) => (
+      <Headline1 onAccent bold={bold}>
+        {children}
+      </Headline1>
     ),
-    [BLOCKS.HEADING_2]: (node, children) => (
-      <Headline2 onAccent>{children}</Headline2>
+    [BLOCKS.HEADING_2]: (node, children, bold) => (
+      <Headline2 onAccent bold={bold}>
+        {children}
+      </Headline2>
     ),
-    [BLOCKS.HEADING_3]: (node, children) => (
-      <Headline3 onAccent>{children}</Headline3>
+    [BLOCKS.HEADING_3]: (node, children, bold) => (
+      <Headline3 onAccent bold={bold}>
+        {children}
+      </Headline3>
     ),
-    [BLOCKS.HEADING_4]: (node, children) => (
-      <Headline4 onAccent>{children}</Headline4>
+    [BLOCKS.HEADING_4]: (node, children, bold) => (
+      <Headline4 onAccent bold={bold}>
+        {children}
+      </Headline4>
     ),
-    [BLOCKS.HEADING_5]: (node, children) => (
-      <Headline5 onAccent>{children}</Headline5>
+    [BLOCKS.HEADING_5]: (node, children, bold) => (
+      <Headline5 onAccent bold={bold}>
+        {children}
+      </Headline5>
     ),
-    [BLOCKS.HEADING_6]: (node, children) => (
-      <Headline6 onAccent>{children}</Headline6>
+    [BLOCKS.HEADING_6]: (node, children, bold) => (
+      <Headline6 onAccent bold={bold}>
+        {children}
+      </Headline6>
     ),
-    [BLOCKS.PARAGRAPH]: (node, children) => {
+    [BLOCKS.PARAGRAPH]: (node, children, bold) => {
       const { value } = node.content[0]
 
       if (value.trim() === '') {
@@ -73,7 +85,12 @@ const options = {
         )
       }
 
-      return <Body2 onAccent>{children}</Body2>
+      console.log('RENDER BOLD BLOCK')
+      return (
+        <Body2 onAccent bold={bold}>
+          {children}
+        </Body2>
+      )
     },
     [BLOCKS.EMBEDDED_ENTRY]: (...props) => {
       console.log('props embedded_entry', props)
@@ -100,11 +117,24 @@ type Props = {}
 
 function renderRichText(data) {
   const components = data.content.reduce((acc, curr, index) => {
-    const block = (
-      <span key={index}>
-        {options.renderNode[curr.nodeType](curr, curr.content[0].value)}
-      </span>
-    )
+    let block
+
+    if (!curr.content.length && curr.file) {
+      block = (
+        <S.Image
+          src={parseContentfulFileUrl(curr.file.file.url)}
+          alt={curr.file.title}
+        />
+      )
+    } else {
+      const bold =
+        curr.content[0].marks.findIndex(({ type }) => type === 'bold') !== -1
+      block = (
+        <span key={index}>
+          {options.renderNode[curr.nodeType](curr, curr.content[0].value, bold)}
+        </span>
+      )
+    }
 
     return [...acc, block]
   }, [])
