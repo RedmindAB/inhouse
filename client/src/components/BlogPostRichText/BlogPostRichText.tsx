09641da7ecd69a970c751c45f7cc73400c25701c
Sascha Ringström <sascha.ringstrom@redmind.se>
2021-06-08 07:19:35 +0200

    reverted rich text renderring

--- client/src/components/BlogPostRichText/BlogPostRichText.tsx
+++ client/src/components/BlogPostRichText/BlogPostRichText.tsx
@@ -1,7 +1,8 @@
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
 import { BLOCKS, MARKS } from '@contentful/rich-text-types'
 import React, { FunctionComponent, useContext } from 'react'
 import { BlogPostContext } from '../../templates/blogPost'
-import { ContentContainer, Spacer } from '../../theme/base'
+import { Spacer } from '../../theme/base'
 import {
   Body2,
   Headline1,
@@ -43,37 +44,25 @@ const options = {
     ),
   },
   renderNode: {
-    [BLOCKS.HEADING_1]: (node, children, bold) => (
-      <Headline1 onAccent bold={bold}>
-        {children}
-      </Headline1>
+    [BLOCKS.HEADING_1]: (node, children) => (
+      <Headline1 onAccent>{children}</Headline1>
     ),
-    [BLOCKS.HEADING_2]: (node, children, bold) => (
-      <Headline2 onAccent bold={bold}>
-        {children}
-      </Headline2>
+    [BLOCKS.HEADING_2]: (node, children) => (
+      <Headline2 onAccent>{children}</Headline2>
     ),
-    [BLOCKS.HEADING_3]: (node, children, bold) => (
-      <Headline3 onAccent bold={bold}>
-        {children}
-      </Headline3>
+    [BLOCKS.HEADING_3]: (node, children) => (
+      <Headline3 onAccent>{children}</Headline3>
     ),
-    [BLOCKS.HEADING_4]: (node, children, bold) => (
-      <Headline4 onAccent bold={bold}>
-        {children}
-      </Headline4>
+    [BLOCKS.HEADING_4]: (node, children) => (
+      <Headline4 onAccent>{children}</Headline4>
     ),
-    [BLOCKS.HEADING_5]: (node, children, bold) => (
-      <Headline5 onAccent bold={bold}>
-        {children}
-      </Headline5>
+    [BLOCKS.HEADING_5]: (node, children) => (
+      <Headline5 onAccent>{children}</Headline5>
     ),
-    [BLOCKS.HEADING_6]: (node, children, bold) => (
-      <Headline6 onAccent bold={bold}>
-        {children}
-      </Headline6>
+    [BLOCKS.HEADING_6]: (node, children) => (
+      <Headline6 onAccent>{children}</Headline6>
     ),
-    [BLOCKS.PARAGRAPH]: (node, children, bold) => {
+    [BLOCKS.PARAGRAPH]: (node, children) => {
       const { value } = node.content[0]
 
       if (value.trim() === '') {
@@ -85,11 +74,7 @@ const options = {
         )
       }
 
-      return (
-        <Body2 onAccent bold={bold}>
-          {children}
-        </Body2>
-      )
+      return <Body2 onAccent>{children}</Body2>
     },
     [BLOCKS.EMBEDDED_ENTRY]: (...props) => {
       console.log('props embedded_entry', props)
@@ -114,33 +99,6 @@ const options = {
 
 type Props = {}
 
-function renderRichText(data) {
-  const components = data.content.reduce((acc, curr, index) => {
-    let block
-
-    if (!curr.content.length && curr.file) {
-      block = (
-        <S.Image
-          src={parseContentfulFileUrl(curr.file.file.url)}
-          alt={curr.file.title}
-        />
-      )
-    } else {
-      const marks = curr.content[0].marks
-      const bold = marks?.findIndex(({ type }) => type === 'bold') !== -1
-      block = (
-        <span key={index}>
-          {options.renderNode[curr.nodeType](curr, curr.content[0].value, bold)}
-        </span>
-      )
-    }
-
-    return [...acc, block]
-  }, [])
-
-  return components
-}
-
 const BlogPostRichText: FunctionComponent<Props> = () => {
   const {
     article: { raw, references },
@@ -160,7 +118,7 @@ const BlogPostRichText: FunctionComponent<Props> = () => {
     return node
   })
 
-  return <S.Container>{renderRichText(data)}</S.Container>
+  return <S.Container>{documentToReactComponents(data, options)}</S.Container>
 }
 
 export default BlogPostRichText
