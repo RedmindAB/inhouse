6a6ecc0408c01c2ab6c50fad9587296d30ea911f
Sacha Ringström <sascha.ringstrom@gmail.com>
2022-05-18 18:29:22 +0200

    add chromium opts

--- functions/src/services/bizwiz/bizwiz.crawler.ts
+++ functions/src/services/bizwiz/bizwiz.crawler.ts
@@ -1,7 +1,13 @@
 import chromium from 'chrome-aws-lambda'
 
 async function crawlForCookie(email: string, password: string) {
-  const browser = await chromium.puppeteer.launch()
+  const browser = await chromium.puppeteer.launch({
+    args: chromium.args,
+    defaultViewport: chromium.defaultViewport,
+    executablePath: await chromium.executablePath,
+    headless: chromium.headless,
+    ignoreHTTPSErrors: true,
+  })
   const page = await browser.newPage()
   await page.goto('https://app.bwz.se/arkitektkopia/login')
   await page.type('#LoginEmail', email)
